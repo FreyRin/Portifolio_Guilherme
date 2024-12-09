9767651d2755eecf436f71f9a180366f294e4c61
FreyRin <guilhermebertoldo58@gmail.com>
2024-12-09 20:48:17 -0300

    [FEATURE] Add Header & Introduction V.0.2.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title:'Página Inicial | Guilherme B',
+        component: HomeComponent
+    }
+];
+    
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { IntroductionComponent } from '../../components/introduction/introduction.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent,IntroductionComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
